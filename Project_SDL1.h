b8f378e96b9ab44c3ac172402bec08d4684d658b
Merlin Votat <m.votat@pickup-services.com>
2021-10-21 17:59:41 +0200

    update

--- Project_SDL1.h
+++ Project_SDL1.h
@@ -14,8 +14,8 @@
 // Defintions
 constexpr double frame_rate = 60.0; // refresh rate
 constexpr double frame_time = 1. / frame_rate;
-constexpr unsigned frame_width = 1400; // Width of window in pixel
-constexpr unsigned frame_height = 900; // Height of window in pixel
+constexpr unsigned frame_width = 1400/2; // Width of window in pixel
+constexpr unsigned frame_height = 900/2; // Height of window in pixel
 // Minimal distance of animals to the border
 // of the screen
 constexpr unsigned frame_boundary = 100;
